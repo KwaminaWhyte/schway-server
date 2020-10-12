1bd4fe048f419479d8cc84d69317a1def475034c
HueyWhyte <stanleyotabil10@gmail.com>
2020-10-12 10:25:59 +0800

    built the search and feed detail page

--- client/src/Pages/Notifications/Notifications.js
+++ client/src/Pages/Notifications/Notifications.js
@@ -10,13 +10,25 @@ export default class Notifications extends Component {
         <TopNavigation pageTitle="Notifications" />
         <div className="nav-spacer"></div>
 
-        <p style={{ fontSize: 23, fontWeight: "bold" }}>
-          Nothing to see here -- yet
-        </p>
+        <section
+          style={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "auto",
+            marginBottom: "auto",
+          }}
+        >
+          <p style={{ fontSize: 23, fontWeight: "bold" }}>
+            Nothing to see here -- yet
+          </p>
 
-        <p style={{ color: "grey", textAlign: "center" }}>
-          When someone mentions you, you'll find it here.
-        </p>
+          <p style={{ color: "grey", textAlign: "center" }}>
+            When someone mentions you, you'll find it here.
+          </p>
+        </section>
       </div>
     );
   }
