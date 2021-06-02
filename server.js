1c36d368a8a37db66ed124550ab43e7dc1a2b69b
HueyWhyte <stanleykwaminaotabil@gmail.com>
2021-06-02 02:37:17 +0000

    changes

--- server.js
+++ server.js
@@ -17,11 +17,15 @@ app.use("/comment", require("./routes/commentRoutes"));
 // "mongodb://127.0.0.1:27017/schway";
 // process.env.MONGODB_URI
 mongoose
-  .connect("mongodb://127.0.0.1:27017/schway", {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-  })
+  .connect(
+    "mongodb+srv://HueyWhyte:Famous10@whyte-wdm4x.mongodb.net/whyte?retryWrites=true&w=majority" ||
+      "mongodb://127.0.0.1:27017/schway",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    }
+  )
   .then(() => console.log("Successfully connected to MongoDB"))
   .catch((err) => console.log(err));
 
